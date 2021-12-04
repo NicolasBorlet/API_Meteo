a3757491baa9444c9880d6b9032bd6fd68ea95fc
NicolasBorlet <75842491+NicolasBorlet@users.noreply.github.com>
2021-12-04 22:37:49 +0100

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
 
-    $("'[class]'").hover(function() {
-        //Mouse enter
-        let current_class = $(this).attr('class');
-        
-        $('[class="'+current_class+'"]').addClass('is-active');
-
-    }, function() {
-        //Mouse leave
-        $('[class]').removeClass('is-active');
-    });
-
+	$(".day").hover(
+        function () {
+          $(this).addClass("is-active");
+        },
+        function () {
+          $(this).removeClass("is-active");
+        }
+      );
 });
