e66b694171bced767e9fdd17db66e0bc7abe13ce
NicolasBorlet <75842491+NicolasBorlet@users.noreply.github.com>
2021-12-05 22:32:04 +0100

    Update main.js -- Ajout API + config

--- www/js/main.js
+++ www/js/main.js
@@ -13,4 +13,21 @@ $(document).ready(function() {
         function () {
             $(this).addClass("is-select");
         });
+
+
+        const APIKEY = "9c1b608f4d3ae3c233ae3f9f51972492";
+
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=Hautecour&units=metric&appid=9c1b608f4d3ae3c233ae3f9f51972492";
+
+        fetch(url).then((response) => 
+          response.json().then((data) => {
+            console.log(data);
+            document.querySelector('#city').innerHTML= data.name;
+            document.querySelector('#temp').innerHTML= data.main.temp +'°';
+            document.querySelector('#wind_speed').innerHTML= data.wind.speed + ' km/h'; //+ " <i class='fas fa-wind'></i>";
+            document.querySelector('#sunrise').innerHTML= data.sys.sunrise; 
+            document.querySelector('#sunset').innerHTML= data.sys.sunset; 
+          })
+        )
+        .catch(err => console.log('Erreur : ' + err));
 });
