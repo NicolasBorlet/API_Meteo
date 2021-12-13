746396906bae6cdb083661b60e2b2012844ed5f9
NicolasBorlet <75842491+NicolasBorlet@users.noreply.github.com>
2021-12-13 12:22:56 +0100

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -13,24 +13,36 @@ $(document).ready(function() {
         function () {
             $(this).addClass("is-select");
         });
-
-
-        const APIKEY = "9c1b608f4d3ae3c233ae3f9f51972492";
-
         
-        //Appel API open weather avec Ville en paramètre
-          let url = "https://api.openweathermap.org/data/2.5/weather?q=Hautecour&units=metric&appid={APIKEY}";
+        const APIKEY = '9c1b608f4d3ae3c233ae3f9f51972492';
+        let city = document.querySelector('#search').value;
 
-        fetch(url)
-          .then((response) => 
-            response.json().then((data) => {
-              console.log(data);
-              document.querySelector('#city').innerHTML= data.name;
-              document.querySelector('#temp').innerHTML= data.main.temp +'°';
-              document.querySelector('#wind_speed').innerHTML= data.wind.speed + ' km/h'; //+ " <i class='fas fa-wind'></i>";
-              document.querySelector('#sunrise').innerHTML= data.sys.sunrise; 
-              document.querySelector('#sunset').innerHTML= data.sys.sunset; 
-            })
-        )
-        .catch(err => console.log('Erreur : ' + err));
+        //Appel API open weather avec Ville en paramètre
+        let apiCall = function (city){
+          let url = 'https://api.openweathermap.org/data/2.5/weather?q="${"city"}&units=metric&appid=9c1b608f4d3ae3c233ae3f9f51972492';
+
+          fetch(url)
+            .then((response) => 
+              response.json().then((data) => {
+                console.log(data);
+                document.querySelector('#city').innerHTML= data.name;
+                document.querySelector('#temp').innerHTML= data.main.temp +'°';
+                document.querySelector('#wind_speed').innerHTML= data.wind.speed + ' km/h'; //+ " <i class='fas fa-wind'></i>";
+                document.querySelector('#sunrise').innerHTML= data.sys.sunrise; 
+                document.querySelector('#sunset').innerHTML= data.sys.sunset; 
+              })
+          )
+          .catch(err => console.log('Erreur : ' + err));
+        }
+
+        //Ecouteur d'évènement
+        document.querySelector('form').addEventListener('submit', function(e){
+          e.preventDefault();
+          let ville = document.querySelector('#search').value;
+
+          apiCall(ville);
+        })
+
+        //Appel au chargement de la page
+        apicall=('Chambéry');
 });
