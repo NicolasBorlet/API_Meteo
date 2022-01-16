a9150c259aa0236bd8af14c6af2b7e35724d9c89
NicolasBorlet <75842491+NicolasBorlet@users.noreply.github.com>
2022-01-16 12:40:59 +0100

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -11,28 +11,36 @@ $(document).ready(function() {
       $( "button" ).click(
         function () {
             $(this).addClass("is-select");
-          });
+    });
 
-          const settings = {
-            "async": true,
-            "url": "http://localhost/API-Weather/www/weather.php",
-            "method": "GET",
-          };
+
+        //Appel API
+        let apiCall = function (){
+
+          let base_url = "http://localhost/API_Meteo/www/weather.php"
+          let city = document.querySelector('#search').value;
+
           
-          $.ajax(settings).done(function (response) {
-            response= JSON.parse(response);
-            console.log(response.data[0].sunrise);
-            console.log(response.data[0].wind_spd);
-            console.log(response.data[0].city_name);
-            console.log(response.data[0].precip);
-            console.log(response.data[0].sunset);
-          
-            $('#sunrise').text(response.data[0].sunrise);
-            $('#wind_speed').text(response.data[0].wind_spd*3.6+" km/h");
-            $('#city').text(response.data[0].city_name);
-            $('#sunset').text(response.data[0].sunset);
-            $('#humidity').text(response.data[0].precip);
-            $('#temp').text(response.data[0].temp+"°");
-          
-          });
+          fetch(base_url)
+            .then((response) => 
+              response.json().then((data) => {
+                console.log(data);
+                //Je suis obligé de mettre 2x fois data dans mes appels, je pense que la méthode que j'utilise n'est clairemenent pas la plus optimisée
+                document.querySelector('#city').innerHTML = data.city_name;
+                document.querySelector('#temp').innerHTML = data.data[0].temp +'°';
+                document.querySelector('#wind_speed').innerHTML = data.data[0].wind_spd + ' km/h';
+                document.querySelector('#sunrise').innerHTML = data.data[0].sunrise_ts;
+                document.querySelector('#sunset').innerHTML = data.data[0].sunset_ts;
+                document.querySelector('#humidity').innerHTML = data.data[0].precip;
+              })
+          )
+          .catch(err => console.log('Erreur : ' + err));
+        }
+
+        //Ecouteur d'évènement
+        document.querySelector('form').addEventListener('submit', function(e) {
+          e.preventDefault();
+
+          apiCall(city);
+        })
 });
