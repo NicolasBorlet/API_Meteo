ffa94d603c100c49300886994a61a070267406b6
NicolasBorlet <75842491+NicolasBorlet@users.noreply.github.com>
2022-01-17 19:10:05 +0100

    Update weather.php

--- www/weather.php
+++ www/weather.php
@@ -2,8 +2,9 @@
 
 $curl = curl_init();
 
+
 curl_setopt_array($curl, [
-    CURLOPT_URL => "https://api.weatherbit.io/v2.0/forecast/daily?key=d285450c69b44c8a93f6e7f1cb9d4db1&city=Hautecour&lang=fr",
+    CURLOPT_URL => "https://api.weatherbit.io/v2.0/forecast/daily?key=d285450c69b44c8a93f6e7f1cb9d4db1&city=Paris&lang=fr",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
@@ -13,6 +14,9 @@ curl_setopt_array($curl, [
     CURLOPT_POSTFIELDS => "",
 ]);
 
+//Je voualais créer une variable qui allais récupérer la valeur rentre dans l'input "city_name", puis intégrer la variable dans l'url, cela aurait permit à
+//l'utilisateur de pouvoir rechercher la ville qu'il souhaite
+//$city = $_GET["city_name"];
 $response = curl_exec($curl);
 $err = curl_error($curl);
 
